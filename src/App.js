1be727c3fbd9278492a1f2df5bfe70c54c5cace7
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-12-06 21:19:26 -0300

    Rendiza campo de filtro no App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import FilterField from './components/FilterField';
 import Table from './components/Table';
 import { Provider } from './Context/PlanetsProvider';
 
 function App() {
   return (
     <Provider>
+      <FilterField />
       <Table />
     </Provider>
   );
