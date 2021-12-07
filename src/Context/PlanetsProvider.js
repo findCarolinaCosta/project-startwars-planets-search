1f0a955019863f72b7df9e4358238c04ec34f6eb
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-12-06 21:18:02 -0300

    Seta resposta da api no state do PlanetsProvider e passa como data nesse contexto

--- src/Context/PlanetsProvider.js
+++ src/Context/PlanetsProvider.js
@@ -1,19 +1,29 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import fetchPlanets from '../services/fetchPlanets';
 
 const Context = createContext();
 const { Provider, Consumer } = Context;
 
 const INITIAL_CONTEXT = {
-  data,
+  data: [],
 };
 
 function PlanetsProvider({ children }) {
   const [data, setData] = useState(INITIAL_CONTEXT);
+
+  const setDataResponse = async () => {
+    setData(await fetchPlanets());
+  };
+
+  useEffect(() => {
+    setDataResponse();
+  }, []);
+
   const context = {
     data,
-    setData,
   };
+
   return (
     <Provider value={ context }>
       {children}
