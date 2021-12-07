92e562f830ffb10f6e94ca9d455fb36df6a4ca2b
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-12-07 16:02:25 -0300

    Exclui função tableRows e adiciona o retorno como componente

---
+++ src/components/TableRows.js
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function TableRows({ planet }) {
+  return (
+    <>
+      <td>{ planet.name }</td>
+      <td>{ planet.rotation_period }</td>
+      <td>{ planet.orbital_period }</td>
+      <td>{ planet.diameter }</td>
+      <td>{ planet.climate }</td>
+      <td>{ planet.gravity }</td>
+      <td>{ planet.terrain }</td>
+      <td>{ planet.surface_water }</td>
+      <td>{ planet.population }</td>
+      <td>{ planet.films }</td>
+      <td>{ planet.created }</td>
+      <td>{ planet.edited }</td>
+      <td>{ planet.url }</td>
+    </>
+  );
+}
+
+TableRows.propTypes = {
+  planet: PropTypes.shape({
+    climate: PropTypes.string,
+    created: PropTypes.string,
+    diameter: PropTypes.string,
+    films: PropTypes.arrayOf(PropTypes.string),
+    gravity: PropTypes.string,
+    name: PropTypes.string,
+    orbital_period: PropTypes.string,
+    population: PropTypes.string,
+    rotation_period: PropTypes.string,
+    surface_water: PropTypes.string,
+    terrain: PropTypes.string,
+    edited: PropTypes.string,
+    url: PropTypes.string,
+  }).isRequired,
+};
+
+export default TableRows;
