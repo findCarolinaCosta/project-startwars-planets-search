4bf7befd20810b719976b76901d2206d080488e8
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-12-06 21:24:00 -0300

    Adiciona componente funcional Table Que renderiza o cabeçalho da tabela e compoente PlanetInfos com as infos dos planetas

---
+++ src/components/Table.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PlanetInfos from './PlanetInfos';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <PlanetInfos />
+      </table>
+    </div>
+  );
+}
+
+export default Table;
