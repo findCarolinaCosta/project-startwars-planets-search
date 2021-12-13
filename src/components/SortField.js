25029afd31299a2195125e573a7765dbf1bb7c92
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-12 23:53:54 -0300

    Adiciona componente que renderiza o campo de seleção para ordenar

---
+++ src/components/SortField.js
@@ -0,0 +1,70 @@
+import React, { useContext, useState } from 'react';
+import { Context } from '../Context/PlanetsProvider';
+import { INITIAL_OPTIONS_COLUMN } from './FilterField';
+
+function SortField() {
+  const { setOrder } = useContext(Context);
+  const [selectedRadio, setSelectedRadio] = useState('');
+  const [chosenColumn, setChosenColumn] = useState('');
+
+  const handleChange = ({ target }) => {
+    setSelectedRadio(target.value);
+  };
+
+  const handleClick = () => {
+    setOrder((prevState) => ({
+      ...prevState,
+      column: chosenColumn,
+      sort: selectedRadio,
+    }));
+  };
+
+  return (
+    <div>
+      <label htmlFor="option__order">
+        Ordenar:
+        <select
+          id="option__order"
+          data-testid="column-sort"
+          name="column"
+          onChange={ ({ target }) => setChosenColumn(target.value) }
+          value={ chosenColumn }
+        >
+          {INITIAL_OPTIONS_COLUMN
+            .map((option) => <option key={ option }>{option}</option>)}
+        </select>
+      </label>
+      <label htmlFor="sort__asc">
+        <input
+          name="opcao"
+          id="sort__asc"
+          type="radio"
+          data-testid="column-sort-input-asc"
+          value="ASC"
+          onChange={ handleChange }
+        />
+        Ascendente
+      </label>
+      <label htmlFor="sort__dsc">
+        <input
+          name="opcao"
+          id="sort__dsc"
+          type="radio"
+          data-testid="column-sort-input-desc"
+          value="DESC"
+          onChange={ handleChange }
+        />
+        Descendente
+      </label>
+      <button
+        data-testid="column-sort-button"
+        type="button"
+        onClick={ handleClick }
+      >
+        Ordenar
+      </button>
+    </div>
+  );
+}
+
+export default SortField;
