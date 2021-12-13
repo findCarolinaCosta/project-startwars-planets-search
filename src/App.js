0e6a45f40a27bb6eb67d677aa16413276ac361b6
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-12 23:54:26 -0300

    Renderiza campo de ordernar no app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import FilterField from './components/FilterField';
+import SortField from './components/SortField';
 import Table from './components/Table';
 import { Provider } from './Context/PlanetsProvider';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Provider>
       <FilterField />
+      <SortField />
       <Table />
     </Provider>
   );
