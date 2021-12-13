b96eebeb27eb6f6ac76c2491153eca309842c817
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-13 00:33:30 -0300

    Adiciona comentários para cada filtro e ou ordenação

--- src/components/PlanetInfos.js
+++ src/components/PlanetInfos.js
@@ -18,8 +18,10 @@ function PlanetInfos() {
     <tbody>
       {(!filtered && filterByName === '' && !order.column) ? (
         data
+          // ordena como padrão os planetas
           .sort((prevPlanet, nextPlanet) => prevPlanet
             .name.localeCompare(nextPlanet.name))
+          // retorna as linhas da tabela
           .map((planet) => (
             <tr key={ Math.random().toString().substr(2, maxDecimalPlaces) }>
               <TableRows planet={ planet } />
@@ -28,10 +30,14 @@ function PlanetInfos() {
       )
         : (
           data
+            // filtra por valores numéricos
             .filter((planet) => getFilterByNumericValues(planet, filterByNumericValues))
+            // filtra pelo nome
             .filter(({ name }) => (filterByName !== ''
               ? name.includes(filterByName) : true))
+            // ordena planetas filtrados ou não
             .sort((prevPlanet, nextPlanet) => getSort(prevPlanet, nextPlanet, order))
+            // retorna as linhas da tabela para cada item do array filtrado/ordenado
             .map((planetFilterByNumeric) => (
               <tr key={ Math.random().toString().substr(2, maxDecimalPlaces) }>
                 <TableRows planet={ planetFilterByNumeric } />
