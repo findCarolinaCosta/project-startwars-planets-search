237b0325d3f034cf40152e0a5b5ac3d41a29e787
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-18 08:23:47 -0300

    Adiciona estilização final 😀

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import FilterField from './components/FilterField';
+import LoadingInitial from './components/LoadingInitial';
 import SortField from './components/SortField';
 import Table from './components/Table';
 import { Provider } from './Context/PlanetsProvider';
@@ -8,12 +9,15 @@ import { Provider } from './Context/PlanetsProvider';
 function App() {
   return (
     <Provider>
-      <section className="section__header font-bold">
-        <FilterField />
-        <SortField />
-      </section>
-      <div className="div__table font-bold">
-        <Table />
+      <LoadingInitial />
+      <div className="container pb-8">
+        <section className="section__header font-bold">
+          <FilterField />
+          <SortField />
+        </section>
+        <div className="div__table font-bold bg-white">
+          <Table />
+        </div>
       </div>
     </Provider>
   );
