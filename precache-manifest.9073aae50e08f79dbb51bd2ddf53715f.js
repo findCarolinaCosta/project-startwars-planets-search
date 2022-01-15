16dead1513311023d2c6fb3468bc8f775952d4f3
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-01-15 20:22:58 -0300

    Updates

---
+++ precache-manifest.9073aae50e08f79dbb51bd2ddf53715f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3dac4cd1ae9e950843372ed01bfeb9",
+    "url": "/project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "06c70d4dc6517db469c9",
+    "url": "/project-starwars-planets-search/static/css/main.85661e33.chunk.css"
+  },
+  {
+    "revision": "617cdfdb882c7c33c08b",
+    "url": "/project-starwars-planets-search/static/js/2.277afe01.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-starwars-planets-search/static/js/2.277afe01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c70d4dc6517db469c9",
+    "url": "/project-starwars-planets-search/static/js/main.204a91c8.chunk.js"
+  },
+  {
+    "revision": "f7eb459dd634f26d2408",
+    "url": "/project-starwars-planets-search/static/js/runtime-main.af1aea79.js"
+  },
+  {
+    "revision": "0c1540f1a0e5c4df85dbe5aedc9a3773",
+    "url": "/project-starwars-planets-search/static/media/background.0c1540f1.jpg"
+  }
+]);
